65d2b37c42f938318864794ae7b0c6f9f2715ffe
Valentin Ruiz Lopez <alurui180@ieselcaminas.org>
2020-12-10 13:37:30 +0100

    exercise 2 anex completed

---
+++ Practica1_Anexo/Ejercicio5/codigo.js
@@ -0,0 +1,32 @@
+let XMLHttpRequestObject = false
+
+if (window.XMLHttpRequest){
+  XMLHttpRequestObject = new XMLHttpRequest()
+} else if (window.ActiveXObject) {
+  XMLHttpRequestObject = new ActiveXObject('Microsoft.XMLHTTP')
+}
+
+const btnXML = document.getElementById("btn");
+const content = document.getElementById("content")
+
+const btnListener = () => {
+
+  if(XMLHttpRequestObject) {
+    XMLHttpRequestObject.onreadystatechange = function () {
+
+    if (XMLHttpRequestObject.readyState == 4 && XMLHttpRequestObject.status == 200 ) {
+    
+    }
+  }
+    XMLHttpRequestObject.open('GET','', true)
+    XMLHttpRequestObject.send()
+  }
+
+}
+
+const load = (httprequest) => {
+  var responseType = httprequest.responseText;
+
+}
+
+btnXML.addEventListener('click', btnListener)
